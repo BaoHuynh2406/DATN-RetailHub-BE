317131715358e0064ef44e7fec08d22eb23b831a
Tran Dinh Kiet <trantrankiet59@gmail.com>
2024-07-19 16:27:36 +0700

    Kiet_change_sql

--- Database/db_scrip.sql
+++ Database/db_scrip.sql
@@ -1,6 +1,5 @@
-create database retailFlow
 -- Bảng Invoice Items
-CREATE TABLE InvoiceItems (
+CREATE TABLE invoice_items (
     invoice_item_id INT IDENTITY(1,1) PRIMARY KEY, -- Khóa chính tự động tăng
     invoice_id INT, -- Khóa ngoại đến bảng hóa đơn
     product_id INT, -- Khóa ngoại đến bảng sản phẩm
@@ -10,27 +9,29 @@ CREATE TABLE InvoiceItems (
 );
 
 -- Bảng Stock Check
-CREATE TABLE StockCheck (
+CREATE TABLE stock_check (
     stock_check_id INT IDENTITY(1,1) PRIMARY KEY, -- Khóa chính tự động tăng
     employee_id INT, -- Khóa ngoại đến bảng nhân viên
     stock_check_date DATETIME -- Ngày kiểm
 );
 
 -- Bảng Stock Check Item
-CREATE TABLE StockCheckItem (
+CREATE TABLE stock_check_item (
     stock_check_item_id INT IDENTITY(1,1) PRIMARY KEY, -- Khóa chính tự động tăng
     stock_check_id INT, -- Khóa ngoại đến bảng Stock Check
     product_id INT, -- Khóa ngoại đến bảng sản phẩm
     actual_quantity DECIMAL(10,2), -- Số lượng thực tế
     recorded_quantity DECIMAL(10,2), -- Số lượng trong hệ thống
     note NVARCHAR(50), -- Ghi chú
+
 );
 
 -- Bảng Payments
-CREATE TABLE Payments (
+CREATE TABLE payments (
     payment_id INT IDENTITY(1,1) PRIMARY KEY, -- Khóa chính tự động tăng
     invoice_id INT, -- Khóa ngoại đến bảng hóa đơn
     payment_method_id INT, -- Khóa ngoại đến bảng phương thức thanh toán
     amount DECIMAL(18,2), -- Số tiền thanh toán
     payment_date DATETIME, -- Ngày thanh toán
+
 );
