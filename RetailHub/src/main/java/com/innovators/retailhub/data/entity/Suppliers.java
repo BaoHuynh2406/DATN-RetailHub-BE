7158c18001a89dc8f3a9a409f192f5a1b6e0f47c
nguyenhoa102 <lynguyenhoa102@gmail.com>
2024-07-19 17:21:32 +0700

    3 table

---
+++ RetailHub/src/main/java/com/innovators/retailhub/data/entity/Suppliers.java
@@ -0,0 +1,20 @@
+package com.innovators.retailhub.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "suppliers")
+@Data
+public class Suppliers
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int supplierId;
+    private String supplierName;
+    private String supplierDescription;
+    private String supplierPhoneNumber;
+    private String supplierEmail;
+    private String supplierAddress;
+    private boolean active;
+}
