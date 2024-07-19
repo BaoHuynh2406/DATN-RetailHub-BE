e9b6165506eea6307c27f482218bea7994567c8c
BaoHuynh2406 <huynhbaomts2004@gmail.com>
2024-07-19 16:17:04 +0700

    Code base

---
+++ RetailHub/src/main/java/com/innovators/retailhub/RetailHub.java
@@ -0,0 +1,13 @@
+package com.innovators.retailhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailHub {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailHub.class, args);
+    }
+
+}
---
+++ RetailHub/src/test/java/com/innovators/retailhub/RetailHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovators.retailhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
