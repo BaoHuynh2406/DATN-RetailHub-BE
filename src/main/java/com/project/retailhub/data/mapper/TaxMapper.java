b2f032c4f2cf93a189c47cc54c5af9770f1d9185
vinh922004 <superherovhv@gmail.com>
2024-09-11 11:06:32 +0700

    FixTaxMapper_Vinh
    fix lại cho đúng

--- src/main/java/com/project/retailhub/data/mapper/TaxMapper.java
+++ src/main/java/com/project/retailhub/data/mapper/TaxMapper.java
@@ -1,16 +1,27 @@
 package com.project.retailhub.data.mapper;
 
 import com.project.retailhub.data.dto.request.TaxRequest;
+import com.project.retailhub.data.dto.request.UserRequest;
+import com.project.retailhub.data.dto.response.TaxResponse;
+import com.project.retailhub.data.dto.response.UserResponse;
 import com.project.retailhub.data.entity.Tax;
+import com.project.retailhub.data.entity.User;
 import com.project.retailhub.data.repository.TaxRepository;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface TaxMapper {
 
-    @Mapping(source = "taxId", target = "tax", qualifiedByName = "")
-    Tax toTax(TaxRequest request, @Context TaxRepository taxRepository);
+    Tax toTax(TaxRequest request);
+
+    Tax toTaxResponse(TaxResponse response);
+
+    List<TaxResponse> toTaxResponseList(List<Tax> taxes);
+
+    List<Tax> toTaxesList(List<TaxRequest> requests);
 
 }
