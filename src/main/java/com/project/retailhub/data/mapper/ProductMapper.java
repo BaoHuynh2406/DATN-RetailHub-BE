ce22709955eb41b197c4c47ea53ca0179c2b5abc
nguyenhoa102 <lynguyenhoa102@gmail.com>
2024-09-17 15:26:06 +0700

    no

--- src/main/java/com/project/retailhub/data/mapper/ProductMapper.java
+++ src/main/java/com/project/retailhub/data/mapper/ProductMapper.java
@@ -1,4 +1,16 @@
 package com.project.retailhub.data.mapper;
 
-public class ProductMapper {
+import com.project.retailhub.data.dto.request.product.ProductRequest;
+import com.project.retailhub.data.dto.response.product.ProductResponse;
+import com.project.retailhub.data.entity.Product;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    Product toProduct(ProductRequest request);
+    ProductResponse toProductResponse(Product product);
+    List<ProductResponse> toProductResponseList(List<Product> products);
+    List<Product> toProductsList(List<ProductRequest> requests);
 }
